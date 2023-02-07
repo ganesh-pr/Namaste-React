61bc41470f08554652296a04864fef60645b385c
Ganeswara Rao Dodda <pullmerge@gmail.com>
2023-02-07 18:33:07 -0500

    Shimmer and useEffect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import Food_Villa_New from "../../Food_Villa_New.png";
+import { useState } from "react";
 
 const Title = () => {
   return (
@@ -15,17 +16,46 @@ const Title = () => {
 };
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div className="header" style={{ backgroundColor: "yellow" }}>
-      <Title />
-      <ul className="nav-items">
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
+    <>
+      <div className="header" style={{ backgroundColor: "yellow" }}>
+        <Title />
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+        {isLoggedIn ? (
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+        ) : (
+          <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        )}
+      </div>
+    </>
   );
 };
 
+/* Example
+const Header = () => {
+  const [title, setTitle] = useState("Food Villa");
+  console.log("render");
+  return (
+    <>
+      <h1>{title}</h1>
+      <button onClick={() => setTitle("Food Villa 2")}>Change Title</button>
+      <div className="header" style={{ backgroundColor: "yellow" }}>
+        <Title />
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+*/
 export default Header;
---
+++ src/components/Shimmer.js
@@ -0,0 +1,34 @@
+const Shimmer = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+      }}
+    >
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+  // return Array(10).map(() => {
+  //   <div className="card"></div>;
+  // });
+};
+
+export default Shimmer;
