f9c3d494c73ef5e66bdde90dc5e169d4d6d1e522
Ganeswara Rao Dodda <pullmerge@gmail.com>
2023-03-20 05:55:52 -0400

    Style changes

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,7 +2,7 @@ import { IMG_CDN_URL } from "../Constants";
 
 const RestaurantCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
   return (
-    <div className="w-56 p-2 m-2 shadow-lg bg-emerald-50">
+    <div className="w-56 p-2 m-2 shadow-lg bg-purple-50">
       <img src={IMG_CDN_URL + cloudinaryImageId} alt="Rest" />
       <h2 className="font-bold text-xl">{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
